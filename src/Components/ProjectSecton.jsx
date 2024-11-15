77b301e7ff9895b17c605e6aa3659873d94f73bc
SaurabhPatelProgrammer <saurabhprogrammer94@gmail.com>
2024-11-15 17:46:59 +0530

    first commit

--- src/Components/ProjectSecton.jsx
+++ src/Components/ProjectSecton.jsx
@@ -24,6 +24,7 @@ function ProjectSecton() {
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
+        <ProjectCard />
       </div>
       <div className=" flex justify-center items-center gap-3 mt-2 select-none">
         <HiArrowSmLeft
